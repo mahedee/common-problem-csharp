7f4a407b9ad432045d37cbdaf61de7d5a5f9016d
mahedee <mahedee.hasan@gmail.com>
2019-08-11 13:28:35 +0600

    solve gcd using recursion

--- FibonacciRecursion/FibonacciRecursion/Solutions.cs
+++ FibonacciRecursion/FibonacciRecursion/Solutions.cs
@@ -6,6 +6,7 @@ namespace FibonacciRecursion
 {
     public class Solutions
     {
+        //Fibonacci series: 1 1 2 3 5 8 13 21 34 55 .....
         public int Fib(int n)
         {
             if (n == 0 || n == 1)
---
+++ GCDRecursion/GCDRecursion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GCDRecursion
+{
+
+  
+    class Program
+    {
+
+        //GCD = Greatest common divisor
+        public static int GCD(int a, int b)
+        {
+
+            //reminder calculation will be wrong if b is greate than a
+            if(b > a)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            if( a % b == 0)
+            {
+                return b;
+            }
+            else
+            {
+                int reminder = a % b;
+                return GCD(b, reminder);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //GCD of 17 and 5 is 1, GCD of 50 and 10 is 10
+            Console.WriteLine(GCD(17, 5));
+            Console.ReadKey();
+        }
+    }
+}
